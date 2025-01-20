af64777dba4c391cb0263ffecadb35972626a80b
Albert Pieniak <apieniak@c1r7s1.42warsaw.pl>
2025-01-20 18:19:23 +0100

    new version

--- get_next_line.c
+++ get_next_line.c
@@ -3,22 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apieniak <apieniak@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*   By: apieniak <apieniak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 18:46:58 by apieniak          #+#    #+#             */
-/*   Updated: 2025/01/19 18:22:23 by apieniak         ###   ########.fr       */
+/*   Updated: 2025/01/20 16:06:14 by apieniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define BUFFER_SIZE 4
+#define BUFFER_SIZE 42
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 
+int	search_for_char(char *str, char sign)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == sign)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 size_t	ft_strlen(char *str)
 {
-	int 	i;
+	int	i;
 
 	i = 0;
 	while (str[i])
@@ -26,7 +40,7 @@ size_t	ft_strlen(char *str)
 	return (i);
 }
 
-char *ft_strjoin(char *src, char *dest)
+char	*ft_strjoin(char *src, char *dest)
 {
 	size_t	i;
 	size_t	j;
@@ -55,39 +69,81 @@ char *ft_strjoin(char *src, char *dest)
 	return (new);
 }
 
-char *next_line(char *buff);
-
-char *get_line(char *buff)
+char	*next_line(char *buffer)
 {
+	int		i;
+	int		j;
 	char	*line;
 
+	i = 0;
+	// find len of first line
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	// if eol == \0 return NULL
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	// len of file - len of firstline + 1
+	line = malloc((ft_strlen(buffer) - i + 1));
+	i++;
+	j = 0;
+	// line == buffer
+	while (buffer[i])
+		line[j++] = buffer[i++];
+	free(buffer);
 	return (line);
 }
 
-char	*textF_read(char *buffer, int fd)
+char	*get_line(char *buff)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!buff[i])
+		return (NULL);
+	while (buff[i] && buff[i] != '\n')
+		i++;
+	line = malloc(i + 2);
+	i = 0;
+	while (buff[i] && buff[i] != '\n')
+	{
+		line[i] = buff[i];
+		i++;
+	}
+	if (buff[i] && buff[i] == '\n')
+		line[i++] = '\n';
+	return (line);
+}
+
+char	*textf_read(char *buffer, int fd)
 {
 	char	*buff_supp;
-	int	buff_count;
+	int		buff_count;
 
 	buff_count = 1;
 	if (!buffer)
 		buffer = malloc(1);
 	buff_supp = malloc(BUFFER_SIZE + 1);
-	if (buff_supp == NULL)
+	if (!buff_supp)
 		return (NULL);
-	while (buff_count > 1)
+	while (buff_count > 0)
 	{
 		buff_count += read(fd, buff_supp, BUFFER_SIZE);
-		if (buff_count <= 0)
+		if (buff_count == -1)
 		{
-			free (buff_supp);
+			free(buff_supp);
 			return (NULL);
 		}
-		buff_supp[buff_count] = '\0';
+		buff_supp[buff_count] = 0;
 		buffer = ft_strjoin(buffer, buff_supp);
+		if (search_for_char(buffer, '\n'))
+			break ;
 	}
 	free(buff_supp);
-	return(buffer);
+	return (buffer);
 }
 
 char	*get_next_line(int fd)
@@ -97,23 +153,23 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0)
 		return (NULL);
-	buffer = textF_read(buffer, fd);
+	buffer = textf_read(buffer, fd);
 	if (!buffer)
 		return (NULL);
-	line = get_line();
-	buffer = next_line();	
+	line = get_line(buffer);
+	buffer = next_line(buffer);	
 	return (line);
 }
 
 
 int	main(void)
 {
-	int	fd;
+	//int		d;
 	char	*line;
+	int fd;
 
-	fd = open("song.txt", O_RDONLY);
-
-	int i = fd;
+	fd = open("text.txt", O_RDONLY);
+	//int i = fd;
 	if (fd < 0)
 	{
 		printf("Error");
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apieniak <apieniak@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*   By: apieniak <apieniak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 19:01:00 by apieniak          #+#    #+#             */
-/*   Updated: 2025/01/14 19:01:16 by apieniak         ###   ########.fr       */
+/*   Updated: 2025/01/20 15:41:14 by apieniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+
+int		search_for_char(char *str, char sign);
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char *src, char *dest);
+char	*next_line(char *buff);
+char	*get_line(char *buff);
+char	*textf_read(char *buffer, int fd);
+char	*get_next_line(int fd);
+
+#endif
