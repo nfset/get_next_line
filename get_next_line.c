b2c851f60ce3ba829ca2102d3d46d769da1a43f1
Albert Pieniak <zs.a.pieniak@gmail.com>
2025-01-22 16:04:16 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,88 +6,11 @@
 /*   By: apieniak <apieniak@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 18:46:58 by apieniak          #+#    #+#             */
-/*   Updated: 2025/01/22 15:41:32 by apieniak         ###   ########.fr       */
+/*   Updated: 2025/01/22 15:55:14 by apieniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define BUFFER_SIZE 42
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <stdint.h>
-
-void	*my_calloc(int elements, int size)
-{
-	void	*arr;
-	char	*swap;
-	int		i;
-
-	i = 0;
-	arr = (void *)malloc(elements * size);
-	if (!arr)
-		return (NULL);
-	swap = (char *)arr;
-	while (i < elements * size)
-	{
-		swap[i] = 0;
-		i++;
-	}
-	return (arr);
-}
-
-int	search_for_char(char *str, char sign)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == sign)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-size_t	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char *src, char *dest)
-{
-	size_t	i;
-	size_t	j;
-	char	*new;
-
-	if (!src || !dest)
-		return (NULL);
-	new = my_calloc(ft_strlen(src) + ft_strlen(dest) + 1, 1);
-	if (!new)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (src[i] != '\0')
-	{
-		new[i] = src[i];
-		i++;
-	}
-	while (dest[j] != '\0')
-	{
-		new[i] = dest[j];
-		i++;
-		j++;
-	}
-	free(src);
-	new[i] = '\0';
-	return (new);
-}
+#include "get_next_line.h"
 
 char	*next_line(char *buffer)
 {
@@ -141,7 +64,7 @@ char	*textf_read(char *buffer, int fd)
 
 	buff_count = 1;
 	if (!buffer)
-		buffer = malloc(1);
+		buffer = my_calloc(1, 1);
 	buff_supp = my_calloc(BUFFER_SIZE + 1, 1);
 	while (buff_count > 0)
 	{
@@ -175,27 +98,3 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-
-int	main(void)
-{
-	int	fd;
-	char	*line;
-
-	fd = open("kiedy.txt", O_RDONLY);
-
-	if (fd < 0)
-	{
-		printf("Error");
-		return (1);
-	}
-	line = get_next_line(fd);
-	while (line != NULL)
-	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-	printf("Value of fd: %d", fd);
-	close(fd);
-	return (0);
-}
